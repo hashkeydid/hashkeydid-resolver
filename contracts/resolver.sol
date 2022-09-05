f7f68c0f905cfcbbd2fc8aceef8a856d419c5f3b
xuzhiming <xuzhiming@hashquark.io>
2022-09-05 16:39:55 +0800

    2.0.0

--- contracts/resolver.sol
+++ contracts/resolver.sol
@@ -2,7 +2,14 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/proxy/utils/Initializable.sol";
-import "./resolverStorage.sol";
+import "./ResolverStorage.sol";
+
+interface DID {
+    function isAddrAuthorized(uint256 tokenId, address addr) external view returns (bool);
+    function tokenOfOwnerByIndex(address owner, uint256 index) external view returns (uint256);
+    function ownerOf(uint256 tokenId) external view returns (address);
+    function tokenId2Did(uint256 tokenId) external view returns (string memory);
+}
 
 contract Resolver is ResolverStorage, Initializable {
 
@@ -14,12 +21,10 @@ contract Resolver is ResolverStorage, Initializable {
     event PubkeyChanged(uint256 indexed tokenId, bytes32 x, bytes32 y);
     /// @dev Emitted when change text successfully 
     event TextChanged(uint256 indexed tokenId, string key, string value);
-    /// @dev Emitted when the reverse status is changed
-    event ReverseChanged(address indexed addr, bool isReverse);
 
     /// @dev Permits modifications only by the owner of tokenId
     modifier authorized(uint256 tokenId) {
-        require(did.ownerOf(tokenId) == msg.sender, "authorize fail");
+        require(DID(did).ownerOf(tokenId) == msg.sender, "authorize fail");
         _;
     }
 
@@ -31,38 +36,37 @@ contract Resolver is ResolverStorage, Initializable {
         public 
         initializer 
     {
-        did = DID(DIDAddr);
+        did = DIDAddr;
     }
 
     /// @dev Set whether reverse the address to did
     /// @param _addr address of user
     /// @param isReverse true/false
     function setReverse(address _addr, bool isReverse) public {
-        require(_addr == msg.sender, "authorize fail");
+        require(msg.sender == _addr, "authorize fail");
         _isReverse[_addr] = isReverse;
-        emit ReverseChanged(_addr, isReverse);
     }
 
     /// @dev Get the did name from the addr
     /// @param _addr address of user
     function name(address _addr) public view returns (string memory) {
         require(_isReverse[_addr], "this addr has not set reverse record");
-        uint256 tokenId = did.tokenOfOwnerByIndex(_addr, 0);
-        return did.tokenId2Did(tokenId);
+        uint256 tokenId = DID(did).tokenOfOwnerByIndex(_addr, 0);
+        return DID(did).tokenId2Did(tokenId);
     }
 
     /// @dev Sets DID different address on different chain
-    /// @param tokenId The tokenId to query
-    /// @param coinType The type of chains
-    /// @param _addr The chain's address(bytes type)
+    /// @param tokenId the tokenId to query
+    /// @param coinType the type of chains
+    /// @param _addr the chain's address(bytes type)
     function setAddr(uint256 tokenId, uint256 coinType, bytes memory _addr) public authorized(tokenId) {
         _addresses[tokenId][coinType] = _addr;
         emit AddressChanged(tokenId, coinType, _addr);
     }
 
     /// @dev Check address on different chain
-    /// @param tokenId The tokenId to query
-    /// @param coinType The type of chains
+    /// @param tokenId the tokenId to query
+    /// @param coinType the type of chains
     function addr(uint256 tokenId, uint256 coinType) public view returns(bytes memory) {
         return _addresses[tokenId][coinType];
     }
@@ -82,32 +86,32 @@ contract Resolver is ResolverStorage, Initializable {
     }
 
     /// @dev Sets the SECP256k1 public key associated with a did
-    /// @param tokenId The tokenId to query
-    /// @param x The X coordinate of the curve point for the public key
-    /// @param y The Y coordinate of the curve point for the public key
+    /// @param tokenId the tokenId to query
+    /// @param x the X coordinate of the curve point for the public key
+    /// @param y the Y coordinate of the curve point for the public key
     function setPubkey(uint256 tokenId, bytes32 x, bytes32 y) external authorized(tokenId) {
         _pubkeys[tokenId] = PublicKey(x, y);
         emit PubkeyChanged(tokenId, x, y);
     }
 
     /// @dev Gets the SECP256k1 public key associated with a did
-    /// @param tokenId The tokenId to query
+    /// @param tokenId the tokenId to query
     function pubkey(uint256 tokenId) external view returns (bytes32 x, bytes32 y) {
         return (_pubkeys[tokenId].x, _pubkeys[tokenId].y);
     }
 
     /// @dev Sets the text information associated with a did
-    /// @param tokenId The tokenId to query
-    /// @param key The key of info
-    /// @param value The value of info
+    /// @param tokenId the tokenId to query
+    /// @param key the key of info
+    /// @param value the value of info
     function setText(uint256 tokenId, string calldata key, string calldata value) external authorized(tokenId) {
         _texts[tokenId][key] = value;
         emit TextChanged(tokenId, key, value);
     }
 
     /// @dev Gets the text information associated with a did
-    /// @param tokenId The tokenId to query
-    /// @param key The key of info
+    /// @param tokenId the tokenId to query
+    /// @param key the key of info
     function text(uint256 tokenId, string calldata key) external view returns (string memory) {
         return _texts[tokenId][key];
     }
--- contracts/resolverStorage.sol
+++ contracts/resolverStorage.sol
@@ -1,16 +1,9 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
 pragma solidity ^0.8.0;
 
-interface DID {
-    function isAddrAuthorized(uint256 tokenId, address addr) external view returns (bool);
-    function tokenOfOwnerByIndex(address owner, uint256 index) external view returns (uint256);
-    function ownerOf(uint256 tokenId) external view returns (address);
-    function tokenId2Did(uint256 tokenId)external view returns (string memory);
-}
-
 contract ResolverStorage {
 
-    DID did;
+    address did;
 
     mapping(uint256 => mapping(uint256 => bytes)) _addresses;
 
@@ -22,6 +15,7 @@ contract ResolverStorage {
         bytes32 x;
         bytes32 y;
     }
+    
     mapping(uint256 => PublicKey) _pubkeys;
 
     mapping(uint256 => mapping(string => string)) _texts;
